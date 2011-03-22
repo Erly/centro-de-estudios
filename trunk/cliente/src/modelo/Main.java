5da92e64c9bcea2dfd48ec677c6f567e4cf77a67
Erlantz8@gmail.com <Erlantz8@gmail.com@726ba7c7-c736-f299-e4da-1cb74701637d>
2011-03-22 22:59:59 +0000

    Cliente: Importadas las clases 'Peticion' y 'Respuesta'. Activado el Login y añadido un boton temporal para introducir datos del servidor

--- trunk/cliente/src/modelo/Main.java
+++ trunk/cliente/src/modelo/Main.java
@@ -1,6 +1,11 @@
 package modelo;
 
+import interfaz.VLogin;
 import interfaz.VPrincipal;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 import java.sql.SQLException;
 import javax.swing.UIManager;
 import datos.AccesoBD;
@@ -10,21 +15,25 @@ public class Main {
 	
 	public static CentroDB db = new CentroDB();
 	public static CentroEstudios centroEstudios = new CentroEstudios();
+	public static Socket socket;
+	public static ObjectInputStream in;
+	public static ObjectOutputStream out;
 
 	public static void main(String[] args) {
 		try{
 			UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
-			db.abrirConexionSinODBC("//erlantz.x10.mx/erly_centrodb", AccesoBD.MYSQL, "erly_root", "administrador69");
+			/*db.abrirConexionSinODBC("//localhost/centrodb", AccesoBD.MYSQL, "root", "administrador");
 		}catch(SQLException e) {
-			System.out.println("No se ha podido establecer la conexión.");
+			System.out.println("No se ha podido establecer la conexión.");*/
 		}catch(ClassNotFoundException e) {
 			e.printStackTrace();
 		}catch(Exception e){
 			System.out.println(e.getMessage());
 		}
-
-		VPrincipal vprincipal = new VPrincipal();
-		vprincipal.setVisible(true);
+		VLogin vlogin = new VLogin();
+		vlogin.setVisible(true);
+		//VPrincipal vprincipal = new VPrincipal();
+		//vprincipal.setVisible(true);
 	}
 	
 	/**
@@ -40,4 +49,6 @@ public class Main {
 			e.printStackTrace();
 		}
 	}
+	
+	
 }
---
+++ trunk/cliente/src/modelo/Respuesta.java
@@ -0,0 +1,42 @@
+package modelo;
+
+import java.io.Serializable;
+import java.util.Hashtable;
+import java.util.Vector;
+
+import modelo.Hardware.Hardware;
+
+public class Respuesta implements Serializable{
+	
+	public Boolean exito;
+	public String mensaje;
+	public Hardware hardware;
+	public Vector resultado;
+	public Hashtable resultado2;
+
+	public Respuesta() {}
+	
+	public Respuesta(Boolean exito){
+		this.exito=exito;
+	}
+	
+	public Respuesta(Boolean exito, String mensaje){
+		this(exito);
+		this.mensaje=mensaje;
+	}
+	
+	public Respuesta(Boolean exito, String mensaje, Hardware resultado){
+		this(exito,mensaje);
+		this.hardware=resultado;
+	}
+	
+	public Respuesta(Boolean exito, String mensaje, Vector resultado){
+		this(exito,mensaje);
+		this.resultado=resultado;
+	}
+	
+	public Respuesta(Boolean exito, String mensaje, Hashtable resultado){
+		this(exito,mensaje);
+		this.resultado2=resultado;
+	}
+}
