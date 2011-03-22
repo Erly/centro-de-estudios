8492f09af8c22e81d310f91495f3a2c4b5a90010
Erlantz8@gmail.com <Erlantz8@gmail.com@726ba7c7-c736-f299-e4da-1cb74701637d>
2011-03-22 22:59:59 +0000

    Cliente: Importadas las clases 'Peticion' y 'Respuesta'. Activado el Login y añadido un boton temporal para introducir datos del servidor

--- cliente/src/interfaz/VLogin.java
+++ cliente/src/interfaz/VLogin.java
@@ -3,6 +3,7 @@ package interfaz;
 import java.awt.BorderLayout;
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.UIManager;
@@ -13,6 +14,20 @@ import javax.swing.ImageIcon;
 import javax.swing.JTextField;
 import java.awt.Color;
 import javax.swing.JCheckBox;
+
+import modelo.Main;
+import modelo.Peticion;
+import modelo.Respuesta;
+import modelo.Usuarios.Usuario;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 @SuppressWarnings({"serial"})
 public class VLogin extends JDialog {
@@ -60,6 +75,28 @@ public class VLogin extends JDialog {
 		contentPanel.add(lblLogin);
 		{
 			JButton okButton = new JButton("OK");
+			okButton.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					String usuario = txtUsuario.getText();
+					String pass = txtPassword.getText();
+					try {
+						Main.out.writeObject(new Peticion(new Usuario(usuario, pass, "")));
+						Respuesta respuesta = (Respuesta)Main.in.readObject();
+						JOptionPane.showMessageDialog(null, respuesta.mensaje, "Login", JOptionPane.DEFAULT_OPTION);
+						if(respuesta.exito){
+							
+						}
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} catch (ClassNotFoundException e2) {
+						// TODO Auto-generated catch block
+						e2.printStackTrace();
+					}
+					
+				}
+			});
 			okButton.setBounds(287, 264, 54, 24);
 			contentPanel.add(okButton);
 			okButton.setActionCommand("OK");
@@ -71,6 +108,37 @@ public class VLogin extends JDialog {
 			contentPanel.add(cancelButton);
 			cancelButton.setActionCommand("Cancel");
 		}
+		
+		JButton btnSeleccionarServidor = new JButton("Seleccionar Servidor");
+		btnSeleccionarServidor.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				String ip = null;
+				int puerto = 0;
+				while(!comprobarip(ip)){
+					ip = JOptionPane.showInputDialog("Introduce la ip del servidor: ");
+					if(ip==null){
+						
+					}
+				}
+				while(puerto < 1 || puerto > 65535){
+					puerto = Integer.parseInt(JOptionPane.showInputDialog("Introduce el puerto del servidor <1-65535>: "));
+				}
+				try {
+					Main.socket = new Socket(ip, puerto);
+					Main.out = new ObjectOutputStream(Main.socket.getOutputStream());
+					Main.in = new ObjectInputStream(Main.socket.getInputStream());
+				} catch (UnknownHostException e1) {
+					JOptionPane.showMessageDialog(null, "No es posible encontrar un servidor en la dirección introducida.", "Host desconocido", JOptionPane.ERROR_MESSAGE);
+				} catch (ConnectException ce){
+					JOptionPane.showMessageDialog(null, "La conexión ha sido rechazada por el servidor. Asegurese de arrancar el servidor con los permisos adecuados.", "Host desconocido", JOptionPane.ERROR_MESSAGE);
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		btnSeleccionarServidor.setBounds(12, 264, 182, 25);
+		contentPanel.add(btnSeleccionarServidor);
 		{
 			JLabel lblUsuario = new JLabel("Usuario");
 			lblUsuario.setForeground(Color.WHITE);
@@ -123,5 +191,54 @@ public class VLogin extends JDialog {
 			Fondo.setBounds(0, 0, 450, 298);
 			contentPanel.add(Fondo);
 		}
+	}
+	
+	public boolean comprobarip(String ip){
+		if(ip == null){
+			return false;
+		}
+		System.out.println(ip);
+		String octetos[] = ip.split("\\.");
+		System.out.println(octetos[0]);
+		System.out.println(octetos[1]);
+		System.out.println(octetos[2]);
+		System.out.println(octetos[3]);
+		if (octetos.length != 4){
+			System.out.println("octetos");
+			JOptionPane.showMessageDialog(null, "La IP introducida es incorrecta.", "IP incorrecta", JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+		int uno,dos,tres,cuatro;
+		try{
+			uno = Integer.parseInt(octetos[0]);
+			dos = Integer.parseInt(octetos[1]);
+			tres = Integer.parseInt(octetos[2]);
+			cuatro = Integer.parseInt(octetos[3]);
+			System.out.println("" + uno);
+			System.out.println("" + dos);
+			System.out.println("" + tres);
+			System.out.println("" + cuatro);
+		}catch(NumberFormatException e){
+			System.out.println("NumberFormatException");
+			JOptionPane.showMessageDialog(null, "La IP introducida es incorrecta.", "IP incorrecta", JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+		if(uno < 1 || uno > 255){
+			JOptionPane.showMessageDialog(null, "La IP introducida es incorrecta.", "IP incorrecta", JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+		if(dos < 0 || dos > 255){
+			JOptionPane.showMessageDialog(null, "La IP introducida es incorrecta.", "IP incorrecta", JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+		if(tres < 0 || tres > 255){
+			JOptionPane.showMessageDialog(null, "La IP introducida es incorrecta.", "IP incorrecta", JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+		if(cuatro < 0 || cuatro > 255){
+			JOptionPane.showMessageDialog(null, "La IP introducida es incorrecta.", "IP incorrecta", JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+		return true;
 	}
 }
