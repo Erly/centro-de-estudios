9e48abc008262bc14f7d8354bd199fa45d8c10ab
Erlantz8@gmail.com <Erlantz8@gmail.com@726ba7c7-c736-f299-e4da-1cb74701637d>
2011-03-26 15:34:50 +0000

    Servidor: Copiado y adaptado el Build.xml del cliente y creados los directorios necesarios además de la clase 'VERSION'

---
+++ trunk/servidor/src/datos/VERSION.java
@@ -0,0 +1,9 @@
+package datos;
+
+public class VERSION{
+    //Clase auto-generada por el script Build.xml usando ant.
+    public static final int MAJOR = 0;
+    public static final int MINOR = 3;
+	public static final int BUILD = 20110326;
+}
+		
